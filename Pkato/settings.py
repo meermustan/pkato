31a5114d91e13e8c451c6c2f73c15e4e2089af6e
Meer Mustan <77464169+meermustan@users.noreply.github.com>
2021-09-07 15:35:11 +0500

    change media root in setting.py

--- Pkato/settings.py
+++ Pkato/settings.py
@@ -129,7 +129,7 @@ MESSAGE_TAGS = {
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_ROOT= os.path.join(BASE_DIR, "media")
+MEDIA_ROOT= BASE_DIR / 'media'
 MEDIA_URL="/media/"
 
 # Default primary key field type
