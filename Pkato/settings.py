0bfd0abbc9b1c88d075417d09d0143d21976685b
Meer Mustan <77464169+meermustan@users.noreply.github.com>
2023-02-17 10:53:56 +0500

    set DEBUG to True because media files not showing

--- Pkato/settings.py
+++ Pkato/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # '127.0.0.1'
 ALLOWED_HOSTS = ['pkato.herokuapp.com','localhost','pkato-git.herokuapp.com','pkato.up.railway.app','www.pkato.up.railway.app']
 
